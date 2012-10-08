939beacb4af0038c5245a4755756ac97f3287401
Andrew Edward Gall <housepage@gmail.com>
2012-10-07 22:05:33 -0700

    Working on database stuff

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var express = require('express')
   , path = require('path')
   , everyauth = require('everyauth');
 
-var connection_string_regex = /^postgres://([a-z]+):([0-9a-zA-Z\_]+)@([a-z0-9\.]+.amazonaws.com):([0-9]+)/([a-zA-Z0-9]+)$/;
+var connection_string_regex = /^postgres:\/\/([a-z]+):([0-9a-zA-Z\_]+)@([a-z0-9\.]+.amazonaws.com):([0-9]+)\/([a-zA-Z0-9]+)$/;
 var connection_parts = string.match(connection_string_regex);
 
 console.log(connection_parts);
