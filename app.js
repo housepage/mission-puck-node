94f79f44144662421b934bc3c7f7d82a14d6a16a
Andrew Edward Gall <housepage@gmail.com>
2012-10-08 20:49:04 -0700

    Trying to get google maps to work

--- app.js
+++ app.js
@@ -64,30 +64,7 @@ var sequelize = new Sequelize(database_name, database_username, database_passwor
   pool: { maxConnections: 5, maxIdleTime: 30}
 });
 
-var models = {
-  User : sequelize.define('User', {
-    vegetarian: { 'type': Sequelize.BOOLEAN , 'defaultValue': true }
-  }),
-  FacebookUser: sequelize.define('FacebookUser', {
-    facebook_id: Sequelize.INTEGER,
-    name: Sequelize.STRING,
-    first_name: Sequelize.STRING,
-    last_name: Sequelize.STRING,
-  }),
-  Car: sequelize.define('Car', {
-    make: Sequelize.STRING,
-    model: Sequelize.STRING,
-  }),
-  Location: sequelize.define('Location', {
-    latitude: Sequelize.FLOAT,
-    longitude: Sequelize.FLOAT,
-    cross_streets: Sequelize.STRING
-  }),
-};
-
-models.FacebookUser.belongsTo(models.User);
-models.User.hasMany(models.Car, { as : 'Cars' } );
-models.Car.hasMany(models.Location);
+var models = require('./models.js');
 
 sequelize.sync()
     .success(function() {})
