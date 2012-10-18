96bb5be17c3e4a4e6304dc4c8d37a897bf79a93b
Andrew Edward Gall <housepage@gmail.com>
2012-10-18 11:41:22 -0700

    Car location lookup almost there

--- routes/locations.js
+++ routes/locations.js
@@ -3,8 +3,8 @@ exports.parked = function(req, res){
   console.log("Session (Keys): " + Object.keys(req.user));
   console.log("Auth: " + req.user.auth);
   console.log("Auth (Keys): " + Object.keys(req.user.auth));
-  console.log("Auth: " + req.user.auth.facebook);
-  console.log("Auth (Keys): " + Object.keys(req.user.auth.facebook));
+  console.log("Facebook Auth: " + req.user.auth.facebook);
+  console.log("Facebook Auth (Keys): " + Object.keys(req.user.auth.facebook));
   req.session.auth.facebook.getCar( function(car){
     res.send(car);
   } );
