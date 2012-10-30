bb3c6918ba55089cdbbbec40c6a0e5b9d3ee5e2c
Andrew Edward Gall <housepage@gmail.com>
2012-10-29 18:02:58 -0700

    Trying to fetch user information from everyauth

--- routes/locations.js
+++ routes/locations.js
@@ -1,9 +1,9 @@
 exports.parked = function(req, res){
-  console.log("Session: " + everyauth);
-  console.log("Session (Keys): " + Object.keys(everyauth));
-  console.log("Auth: " + everyauth.user);
-  console.log("Auth (Keys): " + Object.keys(everyauth.user));
-  everyauth.user.getCar( function(car){
+  console.log("Session: " + req);
+  console.log("Session (Keys): " + Object.keys(req));
+  console.log("Auth: " + req.user);
+  console.log("Auth (Keys): " + Object.keys(req.user));
+  req.user.getCar( function(car){
     res.send(car);
   } );
 };
