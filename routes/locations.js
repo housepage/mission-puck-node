39dc01bb74f502ddf7b6c141a61bc49b841e9ce8
Andrew Edward Gall <housepage@gmail.com>
2012-10-29 17:49:28 -0700

    Trying to get access user information

--- routes/locations.js
+++ routes/locations.js
@@ -5,8 +5,8 @@ exports.parked = function(req, res){
   console.log("Auth (Keys): " + Object.keys(req.session.auth));
   console.log("Facebook Auth: " + req.session.auth.facebook);
   console.log("Facebook Auth (Keys): " + Object.keys(req.session.auth.facebook));
-  console.log("loggedIn: " + req.session.auth.loggedIn);
-  console.log("loggedIn (Keys): " + Object.keys(req.session.auth.loggedIn));
+  console.log("loggedIn: " + req.session.auth.facebook.user);
+  console.log("loggedIn (Keys): " + Object.keys(req.session.auth.facebook.user));
   req.session.auth.facebook.getCar( function(car){
     res.send(car);
   } );
