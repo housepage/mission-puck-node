91c89e48be9dcf82a7a1df9efd1b929992af90fb
Andrew Edward Gall <housepage@gmail.com>
2012-10-29 20:50:54 -0700

    Trying to fetch user information from everyauth

--- database.js
+++ database.js
@@ -66,9 +66,9 @@ var models = {
           onFind([{Hello : "Goodbye"}]); 
         });
       },
-      getCar: function(car_object,onFind) {
-        console.log("In getCar: " + car_object);
-        console.log("In getCar (Keys): " + Object.keys(car_object));
+      getCar: function(onFind) {
+        console.log("In getCar: " + this);
+        console.log("In getCar (Keys): " + Object.keys(this));
         car_object.getCars(function(cars) {
           console.log("Get Cars Found something");
           if(cars.length > 0) {
