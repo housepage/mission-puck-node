2c71fc352da3e20853abdf780cc9f50e9206951e
Andrew Edward Gall <housepage@gmail.com>
2012-10-29 18:00:12 -0700

    Trying to fetch user information from everyauth

--- app.js
+++ app.js
@@ -132,6 +132,8 @@ app.get('/', routes.index);
 app.get('/parked', locations.parked);
 app.get('/users', user.list);
 
+everyauth.helpExpress(app);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
